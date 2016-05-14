5069a79de7b075ddf4d4d437f7a27ac7d092ae94
Petr Stefan <ptr.stef@gmail.com>
2016-05-14 17:22:22 +0200

    Cleanup sources

--- monitor/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.0.2"
-
-__all__ = ['websocket_connections', 'zeromq_connection', 'config_manager']
-
--- monitor/test/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-__all__ = ['test_ClientConnections', 'test_websock_server']
